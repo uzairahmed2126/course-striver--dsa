ea81fb257a801e79acee121fa186f7b6c3c67d0d
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-11-02 00:15:34 +0530

    7 pattern

--- patterns/7.js
+++ patterns/7.js
@@ -1,9 +1,13 @@
 let rows = 5;
 let columns = 9;
 let pattern = "";
-for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < i; j++) {
-    pattern += " ";
+for (let i = 1; i <=rows; i++) {
+  for(let j = i;j<=rows;j++) {
+    pattern+=" "
   }
+  for(let k = 1;k<=2*i-1;k++) {
+    pattern+="*"
+  }
+  pattern+='\n'
 }
 console.log(pattern);
