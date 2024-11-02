5f23aa3810deafcb4d8590d070945cd0e3e8682c
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-11-02 13:08:47 +0530

    8 pattern

--- patterns/8.js
+++ patterns/8.js
@@ -1,23 +1,11 @@
 let rows = 5;
 let columns = 9;
 let pattern = "";
-for (let i = 1; i <= rows; i++) {
-  for (let j = i; j <= rows + 1; j++) {
+for (let i = 0; i < rows; i++) {
+  for (let e = 0; e < i; e++) {
     pattern += " ";
   }
-  for (let k = 1; k <= 2 * i - 1; k++) {
-    pattern += "*";
-  }
-  pattern += "\n";
-}
-for (let i = 1; i <= rows; i++) {
-  for (let j = 0; j <= i; j++) {
-    pattern += " ";
-  }
-  for (let k = i; k <= rows; k++) {
-    pattern += "*";
-  }
-  for (let e = i; e < rows; e++) {
+  for (let j = i; j < columns - i; j++) {
     pattern += "*";
   }
   pattern += "\n";
