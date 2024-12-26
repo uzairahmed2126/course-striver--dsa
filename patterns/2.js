d5002266a004f3a8c3b594de28e505c20daab2d0
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-12-26 17:32:28 +0530

    2

--- patterns/2.js
+++ patterns/2.js
@@ -1,9 +1,9 @@
-let rows = 5;
-let columns = 5;
+let n = 5;
 let pattern = "";
-
-for (let i = 0; i < rows; i++) {
-  pattern += "\n";
+for (let i = 0; i < n; i++) {
+  if (i > 0) {
+    pattern += "\n";
+  }
   for (let j = 0; j < i; j++) {
     pattern += "*";
   }
