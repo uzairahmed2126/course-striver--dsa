47f3e46899a044ee4c44be5f14e3d3f81d2ff28b
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-01-09 15:01:41 +0530

    pattern 19

--- striver aToz/Step 1 : Learn the basics/2.build-up-logical-thinking/19.js
+++ striver aToz/Step 1 : Learn the basics/2.build-up-logical-thinking/19.js
@@ -1,28 +1,32 @@
 function pattern(n) {
   let result = "";
+  let space = 0;
   for (let i = 1; i <= n; i++) {
     for (let j = i; j <= n; j++) {
       result += "*";
     }
-    for (let j = 1; j < i; j++) {
-      result += "  ";
+    for (let j = 1; j <= space; j++) {
+      result += " ";
     }
     for (let j = i; j <= n; j++) {
       result += "*";
     }
+    space += 2;
     if (i !== n) result += "\n";
   }
   result += "\n";
+  space = 2 * n - 2;
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= i; j++) {
       result += "*";
     }
-    for (let j = i+1; j <= n; j++) {
-      result += "  ";
+    for (let j = 1; j <= space; j++) {
+      result += " ";
     }
     for (let j = 1; j <= i; j++) {
       result += "*";
     }
+    space -= 2;
     if (i !== n) result += "\n";
   }
   return result;
